079f4e5de6928dc2b7dba3e8b9795675589d4974
Jay Lu <ta.pnmluan@gmail.com>
2017-03-09 06:51:46 +0700

    Add the-perfect-php-clean-url-generator

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,21 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    setlocale(LC_ALL, 'en_US.UTF8');
+
+	public function toAscii($str, $replace=array(), $delimiter='-') {
+		if( !empty($replace) ) {
+			foreach($replace as $key=>$value) {
+	    	    $str = str_replace($key, $value, $str);
+	    	}
+		} else {
+		    $str = str_replace('đ', 'd', $str);
+		}
+		
+		$clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+		$clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+		$clean = strtolower(trim($clean, '-'));
+		$clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+		return $clean;
+	}
 }
