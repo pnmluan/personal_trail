e7bd54ba880b789c904fc23b1320f1108771b62c
p_hoang <p_hoang@vision-net.co.jp>
2017-03-29 17:26:40 +0700

    save contact

--- public/angular2/src/app/components/contact/contact.component.ts
+++ public/angular2/src/app/components/contact/contact.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AgmCoreModule } from 'angular2-google-maps/core';
 import { Configuration } from '../../shared/app.configuration';
+import { ContactDataService } from '../../shared';
 
 @Component({
 	selector: 'app-contact',
 	templateUrl: './contact.component.html',
+	providers: [ ContactDataService ]
 })
 
 export class ContactComponent implements OnInit {
@@ -13,13 +15,17 @@ export class ContactComponent implements OnInit {
 	lng: number = 106.697295;
 	contact: any = {};
 
-	constructor(){ }
+	constructor(
+		private _ContactDataService: ContactDataService
+	){ }
 
 	ngOnInit(){ }
 
 	onSubmit(form: NgForm){
 		if(form.valid){
-			console.log(this.contact);
+			this._ContactDataService.save(this.contact).subscribe(res => {
+
+			})
 		}
 	}
 
--- public/angular2/src/app/shared/index.ts
+++ public/angular2/src/app/shared/index.ts
@@ -1,4 +1,5 @@
 export * from './article.dataservice';
 export * from './category.dataservice';
+export * from './contact.dataservice';
 export * from './slide.dataservice';
 export * from './tag.dataservice';
